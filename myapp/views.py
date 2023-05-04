2d7605765d4f038788bf475f7ddb1a4b50e88afd
wapol4 <eairist@gmail.com>
2023-05-04 10:04:21 +0700

    Update

--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def hello(request):
-    return HttpResponse("<p>Hello Customer Experience Team</p>")
+    return HttpResponse("<p>Customer Experience Team</p>")
